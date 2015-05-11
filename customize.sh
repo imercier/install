9e7df30950c31120046507fbfefe55f74e0fe584
Ivan Mercier <ivan.mercier@nexvision.fr>
2015-05-11 16:35:17 +0200

    new nex

--- customize.sh
+++ customize.sh
@@ -6,18 +6,10 @@ echo "debian based?"
 
 read foo
 
-sudo apt-get install $(< packages.lst)
-
-mkdir ~/.config/terminator ~/.config/htop
+mkdir -p ~/.config/terminator ~/.config/htop
 cp terminator.config ~/.config/terminator/config
 cp vimrc ~/.vimrc
 cp htoprc ~/.config/htop/htoprc
-cp sh_aliases ~/.sh_aliases
-
-echo "
-if [ -f ~/.sh_aliases ]; then
-    . ~/.sh_aliases
-fi
-" >> ~/.bashrc
+cp sh_aliases ~/.bash_aliases
 
 bash
