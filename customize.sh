9fecef5ed688a093ee3ad8a8fa6770cd9f87119f
Ivan Mercier <ivan.mercier@gmail.com>
2016-09-19 12:36:59 +0200

    packages list, zsh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,32 @@
-#! /bin/bash
+#! /bin/zsh
 
 echo "Have internet?"
-echo "Are you an sudoer?"
-echo "debian based?"
+echo "Running Zsh?"
+echo "R U sudoer?"
 
 read foo
 
-mkdir -p ~/.config/terminator ~/.config/htop
-cp terminator.config ~/.config/terminator/config
-cp .vimrc ~/
-cp htoprc ~/.config/htop/htoprc
-cp .bash_aliases ~/
 
-bash
+PWD=`pwd`
+
+sudo apt-get install $(cat packages.list)
+
+mkdir -p ~/.config/terminator ~/.config/htop
+[ -e ~/.config/terminator.config ] && rm ~/.config/terminator.config
+ln -s $PWD/terminator.config ~/.config/terminator/config
+
+[ -e ~/.config/htop/htoprc ] && rm ~/.config/htop/htoprc
+ln -s $PWD/htoprc ~/.config/htop/htoprc
+
+[ -e ~/.vimrc ] && rm ~/.vimrc
+ln -s $PWD/vimrc ~/.vimrc
+
+ln -s $PWD/aliases ~/.aliases
+
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+[ -e ~/.zshrc ] && rm ~/.zshrc
+ln -s $PWD/zshrc ~/.zshrc
+
+source ~/.zshrc
